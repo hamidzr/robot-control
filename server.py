21ab1de32212b7acf4a1a17571df1fe1ab519769
Hamid Zare <dellydela@gmail.com>
2017-11-16 16:27:45 -0600

    removed leftover comments

--- server.py
+++ server.py
@@ -7,17 +7,12 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'secret')
 socketio = SocketIO(app)
 
 
-def root_dir():  # pragma: no cover
+def root_dir():
     return os.path.abspath(os.path.dirname(__file__))
 
-def get_file(filename):  # pragma: no cover
+def get_file(filename):
     try:
         src = os.path.join(root_dir(), filename)
-        # Figure out how flask returns static files
-        # Tried:
-        # - render_template
-        # - send_file
-        # This should not be so non-obvious
         return open(src).read()
     except IOError as exc:
         return str(exc)
