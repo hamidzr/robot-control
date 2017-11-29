dec3cd3b3d756d421705539f6844bf0e7b86f3a1
Hamid Zare <dellydela@gmail.com>
2017-11-29 17:52:17 -0600

    WIP promisifying plugin, added result status

--- webgme/src/plugins/CommandManager/CommandManager.js
+++ webgme/src/plugins/CommandManager/CommandManager.js
@@ -56,14 +56,23 @@ define([
         // Use self to access core, project, result, logger etc from PluginBase.
         // These are all instantiated at this point.
         var self = this,
-            nodeObject,
+            errors = [],
             activeNode = this.activeNode,
             core = this.core,
-            socket,
             logger = this.logger;
 
+        function setupSocket(address) {
+            return new Promise((resolve, reject) => {
+                require(['/socket.io/socket.io.js'], function(_io) {
+                    let socket = _io(address);
+                    logger.info('finished setting up socket connection.');
+                    resolve(socket);
+                });
+            });
+        }
+
         // emits and object through the socket
-        function sendCmds(cmds) {
+        function sendCmds(socket, cmds) {
             // socket should be setup globally
             if (!socket) handleError('Connection to robot is not ready.');
             cmds = cmds
@@ -87,6 +96,7 @@ define([
         function handleError(msg) {
             console.error(msg);
             logger.info(msg);
+            errors.push(msg);
         }
 
         // returns the targeted robot address
@@ -100,7 +110,7 @@ define([
         };
 
         // locate the startNode in a set of nodes
-        let findStartNode = function(nodes) {
+        function findStartNode(nodes) {
             // TODO add start metanode
             let firstNode;
             firstNode = nodes.find(node => getMetaName(node) === 'Start');
@@ -143,52 +153,56 @@ define([
 
         // check if a nodes is a stop node
         let isStopNode = function(node) {
-            // TODO check using baseType
-            return core.getAttribute(node, 'name') === 'Stop';
+            return getMetaName(node) === 'Stop';
         };
 
-        let cmdChain = [];
         // loads the chain of commands in order from srcNode
-        let loadChain = function(srcNode) {
-            if (!srcNode) throw new Error('trying to load undefined node');
+        function getCommandChain(startNode) {
+            let cmdChain = [];
+            return new Promise((resolve, reject) => {
+                let loadChain = function(srcNode) {
+                    if (!srcNode) throw new Error('trying to load undefined node');
 
-            if (isStopNode(srcNode)) {
-                cmdChain.forEach(cmd => describe(cmd, true));
-                cmdChain = cmdChain.map(stripNode);
-                console.log('Command chain', cmdChain);
-                // call the callback
-                sendCmds(cmdChain);
-                return cmdChain; // promisify? 
-            }
-
-            // generalize to check if is a command node
-            if (core.isConnection(srcNode)) {
-                core.loadPointer(srcNode, 'dst', function (err, destNode) {
-                    if (err) {
-                        handleError(err);
-                        // Handle error
+                    if (isStopNode(srcNode)) {
+                        cmdChain.forEach(cmd => describe(cmd, true));
+                        cmdChain = cmdChain.map(stripNode);
+                        resolve(cmdChain);
+                        return;
                     }
-                    // load the destination
-                    loadChain(destNode);
-                });
-            } else {
-                // add it to command chain
-                cmdChain.push(srcNode);
-                // Load the nodes with pointers named 'src' to the sourceNode.
-                // (Here the connections that have sourceNode as source.)
-                core.loadCollection(srcNode, 'src', function (err, connNodes) {
-                    if (err) {
-                        handleError(err);
+
+                    // generalize to check if is a command node
+                    if (core.isConnection(srcNode)) {
+                        core.loadPointer(srcNode, 'dst', function (err, destNode) {
+                            if (err) {
+                                handleError(err);
+                                // Handle error
+                            }
+                            // load the destination
+                            loadChain(destNode);
+                        });
                     } else {
-                        if (connNodes.length > 1) throw new Error('more than one connection found.');
-                        // load the next one
-                        loadChain(connNodes[0]);
+                        // add it to command chain
+                        cmdChain.push(srcNode);
+                        // Load the nodes with pointers named 'src' to the sourceNode.
+                        // (Here the connections that have sourceNode as source.)
+                        core.loadCollection(srcNode, 'src', function (err, connNodes) {
+                            if (err) {
+                                handleError(err);
+                            } else {
+                                if (connNodes.length > 1) throw new Error('more than one connection found.');
+                                // load the next one
+                                loadChain(connNodes[0]);
+                            }
+                        });
                     }
-                });
-            }
-        };
+                }; // end of loadChain
+                loadChain(startNode);
+
+            });
+        } // end of getCommandChain
         
         core.loadSubTree(activeNode, function (err, nodes) {
+            errors = [];
             let connections = nodes.filter( node => core.isConnection(node));
             nodes = nodes.filter( node => !core.isConnection(node));
             if (nodes.length < 3) handleError('Add more commands.');
@@ -197,31 +211,23 @@ define([
             let robotAddress = getRobotAddress(nodes);
             let startNode = findStartNode(nodes);
             // load socket.io and create a socket connection
-            require(['/socket.io/socket.io.js'], function(_io) {
-                socket = _io(robotAddress);
-                logger.info('finished setting up socket connection.');
-                loadChain(startNode);
-            });
-            
-            if (err) {
-                handleError(err);
-            }
-            // Here we have access to all the nodes that is contained in node
-            // at any level.
+            let socketPromise = setupSocket(robotAddress);
+            let cmdChainPromise = getCommandChain(startNode);
+            Promise.all([socketPromise, cmdChainPromise])
+                .then(([socket, cmdChain]) => {
+                    sendCmds(socket, cmdChain);
+                    // TODO have it wait for a push from the server
+                    self.result.setSuccess(true);
+                    if (errors.length > 0) {
+                        callback(errors.join(', '), self.result);
+                    } else {
+                        callback(null, self.result);
+                    }
+                })
+                .catch(handleError);
         });
-        
-        // This will save the changes. If you don't want to save;
-        // exclude self.save and call callback directly from this scope.
-        self.save('CommandManager updated model.')
-            .then(function () {
-                self.result.setSuccess(true);
-                callback(null, self.result);
-            })
-            .catch(function (err) {
-                // Result success is false at invocation.
-                callback(err, self.result);
-            });
 
+        // callback(null, self.result);
     };
 
     return CommandManager;
